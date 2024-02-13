3fb8a37ec122c055feb9554e2edb448c56f2f681
mehul-bista <mehul.jadav@bistasolutions.com>
2024-02-13 12:04:33 +0530

    [ADD] Odoo Services.

--- addons/owl_demo/__manifest__.py
+++ addons/owl_demo/__manifest__.py
@@ -14,8 +14,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
         'views/res_partner.xml',
+        'views/odoo_services.xml',
     ],
     'demo': [
         'demo/demo.xml',
--- addons/owl_demo/controllers/controllers.py
+++ addons/owl_demo/controllers/controllers.py
@@ -1,21 +1,7 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class OwlDemo(http.Controller):
-#     @http.route('/owl_demo/owl_demo', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/owl_demo/owl_demo/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('owl_demo.listing', {
-#             'root': '/owl_demo/owl_demo',
-#             'objects': http.request.env['owl_demo.owl_demo'].search([]),
-#         })
-
-#     @http.route('/owl_demo/owl_demo/objects/<model("owl_demo.owl_demo"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('owl_demo.object', {
-#             'object': obj
-#         })
+class ResPartner(http.Controller):
+    @http.route('/owl/rpc_service', type='json', auth='user')
+    def get_customers(self, limit):
+        return http.request.env['res.partner'].search_read([], ['name', 'email'], limit=limit)
